ab2ffb6c14b4237b301952d0b627f37ad7e050b9
nayuta-ai <nayuta723@gmail.com>
2023-05-29 05:52:02 +0000

    Add function for adding user and group mappings

--- src/myuts.go
+++ src/myuts.go
@@ -14,6 +14,7 @@ func main() {
 	cmd.Stderr = os.Stderr
 	cmd.Env = []string{"name=shashank"}
 	// command below creates the UTS, PID, IPC, NETWORK and USERNAMESPACES
+	// and adds the user and group mappings
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWUTS |
 			syscall.CLONE_NEWUTS |
@@ -21,6 +22,20 @@ func main() {
 			syscall.CLONE_NEWPID |
 			syscall.CLONE_NEWNET |
 			syscall.CLONE_NEWUSER,
+		UidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 0,
+				HostID:      os.Getuid(),
+				Size:        1,
+			},
+		},
+		GidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 0,
+				HostID:      os.Getgid(),
+				Size:        1,
+			},
+		},
 	}
 	if err := cmd.Run(); err != nil {
 		fmt.Printf("Error running the /bin/bash command - %s\n", err)
