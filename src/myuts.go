a6ec27c6b56f90b8751bd6817df6bc159f3b3d27
nayuta-ai <nayuta723@gmail.com>
2023-05-29 05:42:17 +0000

    Add more namespaces such as uts, pid, ipc, network, usernamespaces

--- src/myuts.go
+++ src/myuts.go
@@ -13,8 +13,14 @@ func main() {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Env = []string{"name=shashank"}
+	// command below creates the UTS, PID, IPC, NETWORK and USERNAMESPACES
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS,
+		Cloneflags: syscall.CLONE_NEWUTS |
+			syscall.CLONE_NEWUTS |
+			syscall.CLONE_NEWIPC |
+			syscall.CLONE_NEWPID |
+			syscall.CLONE_NEWNET |
+			syscall.CLONE_NEWUSER,
 	}
 	if err := cmd.Run(); err != nil {
 		fmt.Printf("Error running the /bin/bash command - %s\n", err)
